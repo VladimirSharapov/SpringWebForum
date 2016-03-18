b713ef4910fb8e51acbbd6a901e33704f274080a
Vladimir Sharapov <vladimirr.sharapov@gmail.com>
2016-03-18 20:00:53 +0300

    javadoc comments to class and method are added

--- webforum-model/src/main/java/org/shv/webforum/model/dao/impl/ExternalLinkDaoImpl.java
+++ webforum-model/src/main/java/org/shv/webforum/model/dao/impl/ExternalLinkDaoImpl.java
@@ -1,12 +1,29 @@
+/**
+ * This project is a simple web forum. I created it just to
+ * demonstrate my programming skills to potential employers.
+ *
+ * Here is short description: ( for more detailed description please reade README.md or
+ * go to https://github.com/VladimirSharapov/SpringWebForum )
+ *
+ * Front-end: jsp, bootstrap, jquery
+ * Back-end: Spring, Hibernate
+ * DB: MySQL and H2(for testing) were used while developing, but the project is database independent.
+ *     Though it must be a relational DB.
+ * Tools: git,maven,jenkins,nexus,liquibase.
+ *
+ * My LinkedIn profile: https://ru.linkedin.com/in/vladimir-sharapov-6075207
+ */
 package org.shv.webforum.model.dao.impl;
 
-import org.hibernate.SessionFactory;
 import org.shv.webforum.common.GenericDao;
 import org.shv.webforum.model.dao.ExternalLinkDao;
 import org.shv.webforum.model.entity.ExternalLink;
 import org.springframework.stereotype.Repository;
 
+import org.hibernate.SessionFactory;
+
 import java.util.List;
+
 
 /**
  *
@@ -23,13 +40,12 @@ public class ExternalLinkDaoImpl extends GenericDao<ExternalLink> implements Ext
         super(sessionFactory, ExternalLink.class);
     }
 
-
     /**
      * {@inheritDoc}
      */
     @Override
+    @SuppressWarnings("unchecked")
     public List<ExternalLink> getAll() {
-
-        return session().createQuery("from ExternalLink").list();
+        return session().createQuery("select e from ExternalLink e").list();
     }
 }
