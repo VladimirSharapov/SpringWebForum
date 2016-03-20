2d7d3586cdb415102e87cdbb35ef2835bbee572f
Vladimir Sharapov <vladimirr.sharapov@gmail.com>
2016-03-20 03:54:54 +0300

    javadoc comments are added

--- webforum-model/src/main/java/org/shv/webforum/util/ExtendedPropertyPlaceholderConfigurer.java
+++ webforum-model/src/main/java/org/shv/webforum/util/ExtendedPropertyPlaceholderConfigurer.java
@@ -1,13 +1,30 @@
+/**
+ * This project is a simple web forum. I created it just to
+ * demonstrate my programming skills to potential employers.
+ *
+ * Here is short description: ( for more detailed description please reade README.md or
+ * go to https://github.com/VladimirSharapov/SpringWebForum )
+ *
+ * Front-end: jsp, bootstrap, jquery
+ * Back-end: Spring, Hibernate
+ * DB: MySQL and H2(for testing) were used while developing, but the project is database independent.
+ *     Though it must be a relational DB.
+ * Tools: git,maven,jenkins,nexus,liquibase.
+ *
+ * My LinkedIn profile: https://ru.linkedin.com/in/vladimir-sharapov-6075207
+ */
 package org.shv.webforum.util;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import java.util.Properties;
+
 
 /**
  *
