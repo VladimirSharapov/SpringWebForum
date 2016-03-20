98ea8777abb1263458b61846fcbb8a5efbd90c93
Vladimir Sharapov <vladimirr.sharapov@gmail.com>
2016-03-20 03:53:09 +0300

    FormData validation group is added

--- webforum-model/src/main/java/org/shv/webforum/model/entity/User.java
+++ webforum-model/src/main/java/org/shv/webforum/model/entity/User.java
@@ -1,24 +1,55 @@
+/**
+ * This project is a simple web forum. I created it just to
+ * demonstrate my programming skills to potential employers.
+ *
+ * Here is short description: ( for more detailed description please reade README.md or
+ * go to https://github.com/VladimirSharapov/SpringWebForum )
+ *
+ * Front-end: jsp, bootstrap, jquery
+ * Back-end: Spring, Hibernate
+ * DB: MySQL and H2(for testing) were used while developing, but the project is database independent.
+ *     Though it must be a relational DB.
+ * Tools: git,maven,jenkins,nexus,liquibase.
+ *
+ * My LinkedIn profile: https://ru.linkedin.com/in/vladimir-sharapov-6075207
+ */
 package org.shv.webforum.model.entity;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.shv.webforum.common.BaseEntity;
+import org.shv.webforum.model.validation.FormData;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import org.hibernate.annotations.NamedQueries;
+import org.hibernate.annotations.NamedQuery;
 import org.hibernate.annotations.Type;
 import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotBlank;
-import org.joda.time.DateTime;
-import org.shv.webforum.common.BaseEntity;
 
+import org.joda.time.DateTime;
+
+import javax.jws.soap.SOAPBinding;
 import javax.persistence.Entity;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.validation.groups.Default;
+import java.util.Collection;
+
 
 /**
  * Stores information about web forum user.
  *
  * @author Vladimir Sharapov
  */
+@NamedQueries({
+        @NamedQuery(name="userByUsername", query = "select u from User u where u.username = :username")
+})
 @Entity
-public class User extends BaseEntity {
+public class User extends BaseEntity implements UserDetails {
 
-    private static final String USER_EMAIL_ILLEGAL_FORMAT = "{validation.invalid_email_format}";
     private static final String USER_PASSWORD_ILLEGAL_LENGTH = "{user.password.length_constraint_violation}";
     private static final String USER_USERNAME_ILLEGAL_LENGTH = "{user.username.length_constraint_violation}";
     private static final String USER_CANT_BE_NULL = "{user.username.null_constraint_violation}";
@@ -30,29 +61,28 @@ public class User extends BaseEntity {
     public static final int USERNAME_MAX_LENGTH = 25;
     public static final int PASSWORD_MIN_LENGTH = 1;
     public static final int PASSWORD_MAX_LENGTH = 50;
-    public static final int USERNAME_FIRSTNAME_MIN_LENGTH = 0;
+    public static final int USERNAME_FIRSTNAME_MIN_LENGTH = 1;
     public static final int USERNAME_FIRSTNAME_MAX_LENGTH = 45;
-    public static final int USERNAME_LASTNAME_MIN_LENGTH = 0;
+    public static final int USERNAME_LASTNAME_MIN_LENGTH = 1;
     public static final int USERNAME_LASTNAME_MAX_LENGTH = 255;
     public static final int EMAIL_MAX_LENGTH = 50;
 
-
     @NotNull(message = USER_CANT_BE_NULL)
-    @Length(min = USERNAME_MIN_LENGTH, max = USERNAME_MAX_LENGTH, message = USER_USERNAME_ILLEGAL_LENGTH)
+    @Size(min = USERNAME_MIN_LENGTH, max = USERNAME_MAX_LENGTH, message = USER_USERNAME_ILLEGAL_LENGTH)
     private String username;
 
     @NotBlank
-    @Length(min = PASSWORD_MIN_LENGTH, max = PASSWORD_MAX_LENGTH, message = USER_PASSWORD_ILLEGAL_LENGTH)
+    @Size(min = PASSWORD_MIN_LENGTH, max = PASSWORD_MAX_LENGTH, message = USER_PASSWORD_ILLEGAL_LENGTH)
     private String password;
 
-    @Length(min = USERNAME_FIRSTNAME_MIN_LENGTH, max = USERNAME_FIRSTNAME_MAX_LENGTH, message = USER_FIRST_NAME_ILLEGAL_LENGTH)
+    @Size(groups = {Default.class,FormData.class}, min = USERNAME_FIRSTNAME_MIN_LENGTH, max = USERNAME_FIRSTNAME_MAX_LENGTH, message = USER_FIRST_NAME_ILLEGAL_LENGTH)
     private String firstName;
 
-    @Length(min = USERNAME_LASTNAME_MIN_LENGTH, max = USERNAME_LASTNAME_MAX_LENGTH, message = USER_LAST_NAME_ILLEGAL_LENGTH)
+    @Size(groups = {Default.class,FormData.class}, min = USERNAME_LASTNAME_MIN_LENGTH, max = USERNAME_LASTNAME_MAX_LENGTH, message = USER_LAST_NAME_ILLEGAL_LENGTH)
     private String lastName;
 
-    @Email(message = USER_EMAIL_ILLEGAL_FORMAT)
-    @Length(max = EMAIL_MAX_LENGTH, message = EMAIL_ILLEGAL_LENGTH)
+    @Email(groups = {Default.class,FormData.class})
+    @Size(groups = {Default.class,FormData.class}, max = EMAIL_MAX_LENGTH, message = EMAIL_ILLEGAL_LENGTH)
     private String email;
 
     @Type(type="org.jadira.usertype.dateandtime.joda.PersistentDateTime")
@@ -67,11 +97,36 @@ public class User extends BaseEntity {
         return username;
     }
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
     /**
      * @param username name that identifies web forum user
      */
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
     }
 
     /**
@@ -156,5 +211,23 @@ public class User extends BaseEntity {
      */
     public void setPostCount(long postCount) {
         this.postCount = postCount;
+    }
+
+    public boolean equals(Object obj) {
+        if (obj == null) { return false; }
+        if (obj == this) { return true; }
+        if (obj.getClass() != getClass()) {
+            return false;
+        }
+        User rhs = (User) obj;
+        return new EqualsBuilder()
+                .append(username,rhs.username)
+                .isEquals();
+    }
+
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(username)
+                .toHashCode();
     }
 }
