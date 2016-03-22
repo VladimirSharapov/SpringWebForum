9c1102bebcc3097a4fa2a0df7049c155423a3b6a
Vladimir Sharapov <vladimirr.sharapov@gmail.com>
2016-03-22 05:51:08 +0300

    javadoc comments are added

--- webforum-service/src/main/java/org/shv/webforum/service/common/BaseEntityService.java
+++ webforum-service/src/main/java/org/shv/webforum/service/common/BaseEntityService.java
@@ -1,11 +1,25 @@
+/**
+ * This project is a simple web forum. I created it just to
+ * demonstrate my programming skills to potential employers.
+ *
+ * Here is short description: ( for more detailed description please reade README.md or
+ * go to https://github.com/VladimirSharapov/SpringWebForum )
+ *
+ * Front-end: jsp, bootstrap, jquery
+ * Back-end: Spring, Hibernate
+ * DB: MySQL and H2(for testing) were used while developing, but the project is database independent.
+ *     Though it must be a relational DB.
+ * Tools: git,maven,jenkins,nexus,liquibase.
+ *
+ * My LinkedIn profile: https://ru.linkedin.com/in/vladimir-sharapov-6075207
+ */
 package org.shv.webforum.service.common;
 
-import org.shv.webforum.service.common.NotFoundException;
 import org.shv.webforum.common.BaseEntity;
+
 
 /**
  * This is generic interface for services which would interact with database entities via DAO object.
- * This interface include all base method declaration which straightly based on database CRUD operations.
  *
  * @author Vladimir Sharapov
  */
@@ -14,10 +28,8 @@ public interface BaseEntityService<T extends BaseEntity> {
     /**
      * Get persistent object by id.
      *
-     * @param id primary id of persistent object to find.
-     *           If id < 0, then IllegalAgrumentEception will be thrown.
-     * @return persistent object T or null if row with primary id  is absent.
-     * @throws org.shv.webforum.service.common.NotFoundException when entity not found
+     * @param id primary id of persistent object to find. If id is less than 0, then IllegalArgumentException will be thrown.
+     * @return persistent object T or throw NotFoundException if object is not found.
      */
     T get(Long id) throws NotFoundException;
 }
--- webforum-service/src/main/java/org/shv/webforum/service/common/BaseEntityServiceImpl.java
+++ webforum-service/src/main/java/org/shv/webforum/service/common/BaseEntityServiceImpl.java
@@ -1,3 +1,18 @@
+/**
+ * This project is a simple web forum. I created it just to
+ * demonstrate my programming skills to potential employers.
+ *
+ * Here is short description: ( for more detailed description please reade README.md or
+ * go to https://github.com/VladimirSharapov/SpringWebForum )
+ *
+ * Front-end: jsp, bootstrap, jquery
+ * Back-end: Spring, Hibernate
+ * DB: MySQL and H2(for testing) were used while developing, but the project is database independent.
+ *     Though it must be a relational DB.
+ * Tools: git,maven,jenkins,nexus,liquibase.
+ *
+ * My LinkedIn profile: https://ru.linkedin.com/in/vladimir-sharapov-6075207
+ */
 package org.shv.webforum.service.common;
 
 import org.shv.webforum.common.BaseEntity;
@@ -5,18 +20,18 @@ import org.shv.webforum.common.Crud;
 
 import java.lang.reflect.ParameterizedType;
 
+
 /**
  * @author Vladimir Sharapov
  */
 public class BaseEntityServiceImpl<T extends BaseEntity, Y extends Crud> implements BaseEntityService<T> {
 
-
-    /*
+    /**
      * dao class that provides crud operations for entity of type T
      */
     Y dao;
 
-    /*
+    /**
      * This constructor can be used by subclasses to inject dao
      *
      * @param dao - dao class that provides crud operations for entity of type T
@@ -34,11 +49,16 @@ public class BaseEntityServiceImpl<T extends BaseEntity, Y extends Crud> impleme
         return dao;
     }
 
-    /*
+    /**
      * {@inheritDoc}
      */
     @Override
+    @SuppressWarnings("unchecked")
     public T get(Long id) throws NotFoundException {
+        if(id < 0) {
+            throw new IllegalArgumentException(String.format("Id [%d] must be positive number",id));
+        }
+
         T entity = (T) dao.get(id);
         if(entity == null) {
             throw new NotFoundException(String.format("Entity %s with id [%d] is not found. ",getEntityClass(),id));
@@ -54,5 +74,4 @@ public class BaseEntityServiceImpl<T extends BaseEntity, Y extends Crud> impleme
         ParameterizedType parameterizedType = (ParameterizedType) getClass().getGenericSuperclass();
         return (Class<?>) parameterizedType.getActualTypeArguments()[0];
     }
-
 }
