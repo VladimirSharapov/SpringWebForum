17112881b3ac9d6e07eb487a75859adb2f86b215
Vladimir Sharapov <vladimirr.sharapov@gmail.com>
2016-03-22 21:24:57 +0300

    getCurrentUser method is added

--- webforum-service/src/main/java/org/shv/webforum/service/transactional/UserServiceImpl.java
+++ webforum-service/src/main/java/org/shv/webforum/service/transactional/UserServiceImpl.java
@@ -1,11 +1,78 @@
+/**
+ * This project is a simple web forum. I created it just to
+ * demonstrate my programming skills to potential employers.
+ *
+ * Here is short description: ( for more detailed description please reade README.md or
+ * go to https://github.com/VladimirSharapov/SpringWebForum )
+ *
+ * Front-end: jsp, bootstrap, jquery
+ * Back-end: Spring, Hibernate
+ * DB: MySQL and H2(for testing) were used while developing, but the project is database independent.
+ *     Though it must be a relational DB.
+ * Tools: git,maven,jenkins,nexus,liquibase.
+ *
+ * My LinkedIn profile: https://ru.linkedin.com/in/vladimir-sharapov-6075207
+ */
 package org.shv.webforum.service.transactional;
 
+import org.shv.webforum.model.dao.UserDao;
+import org.shv.webforum.model.entity.User;
+import org.shv.webforum.service.UserService;
+import org.shv.webforum.service.common.BaseEntityServiceImpl;
+import org.shv.webforum.service.common.NotFoundException;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+
 /**
- * Class contains method needed to manipulate User persistent entity.
- * Also encrypts passwords during account creation, password changing
- * and random password generation.
+ * Service class that contains operations for manipulation with user data (reading, modifying etc)
  *
  * @author Vladimir Sharapov
  */
-public class UserServiceImpl {
+public class UserServiceImpl  extends BaseEntityServiceImpl<User, UserDao> implements UserService {
+
+    protected UserServiceImpl(UserDao dao) {
+        super(dao);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public User getCurrentUser() {
+         return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void editUser(User user) {
+        User editedUser = getDao().get(user.getId());
+
+        editedUser.setFirstName(user.getFirstName());
+        editedUser.setLastName(user.getLastName());
+        editedUser.setEmail(user.getEmail());
+
+        getDao().saveOrUpdate(editedUser);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void updatePostCount() throws NotFoundException {
+        User user = super.get(getCurrentUser().getId());
+
+        user.setPostCount(user.getPostCount() + 1);
+
+        getDao().saveOrUpdate(user);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public User get(Long userId) throws NotFoundException {
+        return super.get(userId);
+    }
 }
