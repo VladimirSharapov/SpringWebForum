e69e07df64e44658d87e923d4c5cbd3788c81869
Vladimir Sharapov <vladimirr.sharapov@gmail.com>
2016-03-22 21:22:27 +0300

    license header is added

--- webforum-service/src/main/java/org/shv/webforum/service/common/NotFoundException.java
+++ webforum-service/src/main/java/org/shv/webforum/service/common/NotFoundException.java
@@ -1,3 +1,18 @@
+/**
+ * This project is a simple web forum. I created it just to
+ * demonstrate my programming skills to potential employers.
+ *
+ * Here is short description: ( for more detailed description please reade README.md or
+ * go to https://github.com/VladimirSharapov/SpringWebForum )
+ *
+ * Front-end: jsp, bootstrap, jquery
+ * Back-end: Spring, Hibernate
+ * DB: MySQL and H2(for testing) were used while developing, but the project is database independent.
+ *     Though it must be a relational DB.
+ * Tools: git,maven,jenkins,nexus,liquibase.
+ *
+ * My LinkedIn profile: https://ru.linkedin.com/in/vladimir-sharapov-6075207
+ */
 package org.shv.webforum.service.common;
 
 /**
