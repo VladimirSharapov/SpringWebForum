698778bb7f509ae3bd013250d327fdf270205192
Vladimir Sharapov <vladimirr.sharapov@gmail.com>
2016-03-23 04:30:38 +0300

    constraint violation test is added

--- webforum-view/webforum-web-controller/src/test/java/org/shv/webforum/controller/ExternalLinkControllerTest.java
+++ webforum-view/webforum-web-controller/src/test/java/org/shv/webforum/controller/ExternalLinkControllerTest.java
@@ -1,6 +1,20 @@
+/**
+ * This project is a simple web forum. I created it just to
+ * demonstrate my programming skills to potential employers.
+ *
+ * Here is short description: ( for more detailed description please reade README.md or
+ * go to https://github.com/VladimirSharapov/SpringWebForum )
+ *
+ * Front-end: jsp, bootstrap, jquery
+ * Back-end: Spring, Hibernate
+ * DB: MySQL and H2(for testing) were used while developing, but the project is database independent.
+ *     Though it must be a relational DB.
+ * Tools: git,maven,jenkins,nexus,liquibase.
+ *
+ * My LinkedIn profile: https://ru.linkedin.com/in/vladimir-sharapov-6075207
+ */
 package org.shv.webforum.controller;
 
-import junit.framework.Assert;
 import org.shv.webforum.controller.util.JsonResponse;
 import org.shv.webforum.controller.util.JsonResponseStatus;
 import org.shv.webforum.model.entity.ExternalLink;
