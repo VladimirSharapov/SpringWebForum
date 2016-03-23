c2526153ea363158fadfb434c68163d52da202e5
Vladimir Sharapov <vladimirr.sharapov@gmail.com>
2016-03-23 04:31:48 +0300

    license header is added

--- webforum-view/webforum-web-controller/src/main/java/org/shv/webforum/controller/ExternalLinkController.java
+++ webforum-view/webforum-web-controller/src/main/java/org/shv/webforum/controller/ExternalLinkController.java
@@ -1,9 +1,25 @@
+/**
+ * This project is a simple web forum. I created it just to
+ * demonstrate my programming skills to potential employers.
+ *
+ * Here is short description: ( for more detailed description please reade README.md or
+ * go to https://github.com/VladimirSharapov/SpringWebForum )
+ *
+ * Front-end: jsp, bootstrap, jquery
+ * Back-end: Spring, Hibernate
+ * DB: MySQL and H2(for testing) were used while developing, but the project is database independent.
+ *     Though it must be a relational DB.
+ * Tools: git,maven,jenkins,nexus,liquibase.
+ *
+ * My LinkedIn profile: https://ru.linkedin.com/in/vladimir-sharapov-6075207
+ */
 package org.shv.webforum.controller;
 
 import org.shv.webforum.controller.util.JsonResponse;
 import org.shv.webforum.controller.util.JsonResponseStatus;
 import org.shv.webforum.model.entity.ExternalLink;
 import org.shv.webforum.service.ExternalLinkService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -11,8 +27,9 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+
 /**
- *  Controller
+ * Controller for dealing with external links
  *
  * @author Vladimir Sharapov
  */
@@ -22,8 +39,7 @@ public class ExternalLinkController {
     private ExternalLinkService service;
 
     /**
-     *
-     * @param service          {@link ExternalLinkService} for link CRUD operations.
+     * @param service  service for link CRUD operations.
      */
     @Autowired
     public ExternalLinkController(ExternalLinkService service) {
@@ -37,7 +53,7 @@ public class ExternalLinkController {
      * @param result link validation result.
      * @return response with SUCCESS status and saved link.
      */
-    @RequestMapping(value = "/links/save", method = RequestMethod.POST)
+    @RequestMapping(value = "/links", method = RequestMethod.POST)
     @ResponseBody
     public JsonResponse saveLink(@Valid @RequestBody ExternalLink link, BindingResult result) {
         if (result.hasErrors()) {
@@ -50,10 +66,10 @@ public class ExternalLinkController {
     /**
      * Delete {@link ExternalLink} with specified id.
      *
-     * @param id link id to deletion.
+     * @param id link id to delete.
      * @return {@code true} if link was successfully deleted.
      */
-    @RequestMapping(value = "/links/delete/{id}", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/links/{id}", method = RequestMethod.DELETE)
     @ResponseBody
     public JsonResponse deleteLink(@PathVariable Long id) {
         boolean result = service.deleteLink(id);
