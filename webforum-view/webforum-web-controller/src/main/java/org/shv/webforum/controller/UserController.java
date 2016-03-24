4fa8f72f73d6f1f97411301b52b25c5d622ef8f4
Vladimir Sharapov <vladimirr.sharapov@gmail.com>
2016-03-24 04:08:09 +0300

    license header is added

--- webforum-view/webforum-web-controller/src/main/java/org/shv/webforum/controller/UserController.java
+++ webforum-view/webforum-web-controller/src/main/java/org/shv/webforum/controller/UserController.java
@@ -1,18 +1,30 @@
+/**
+ * This project is a simple web forum. I created it just to
+ * demonstrate my programming skills to potential employers.
+ *
+ * Here is short description: ( for more detailed description please reade README.md or
+ * go to https://github.com/VladimirSharapov/SpringWebForum )
+ *
+ * Front-end: jsp, bootstrap, jquery
+ * Back-end: Spring, Hibernate
+ * DB: MySQL and H2(for testing) were used while developing, but the project is database independent.
+ *     Though it must be a relational DB.
+ * Tools: git,maven,jenkins,nexus,liquibase.
+ *
+ * My LinkedIn profile: https://ru.linkedin.com/in/vladimir-sharapov-6075207
+ */
 package org.shv.webforum.controller;
 
 import org.shv.webforum.dto.LoginUserDto;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
- * Controller class for dealing with user related requests
+ * Controller  for dealing with user related requests
  *
  * @author Vladimir Sharapov
  */
@@ -22,38 +34,17 @@ public class UserController {
     private static final String LOGIN_USER_DTO = "loginUserDto";
     private static final String REFERER = "referer";
 
-
+    /**
+     * Display login page
+     *
+     * @return {@code ModelAndView} with empty data object for user credentials
+     */
     @RequestMapping(value="/login", method = RequestMethod.GET)
     public ModelAndView loginPage() {
 
         LoginUserDto loginUserDto = new LoginUserDto();
-        loginUserDto.setUserName("Vladimir");
         return new ModelAndView("login").addObject(LOGIN_USER_DTO,loginUserDto).
                                          addObject(REFERER, "http://referer.ru");
 
     }
-
-
-    /**
-     * Handles login action.
-     * @param loginUserDto {@link LoginUserDto} populated in form
-     * @param referer    referer url
-     * @param request    servlet request
-     * @param response   servlet response
-     * @return "success" or "fail" response status
-     */
-  /*  @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public ModelAndView login(LoginUserDto loginUserDto,
-                              @RequestParam(REFERER) String referer,
-                              @RequestParam(value = "_spring_security_remember_me", defaultValue = "off")
-                              String rememberMe,
-                              HttpServletRequest request, HttpServletResponse response) {
-
-
-        System.out.println("username: " + loginUserDto.getUserName());
-        System.out.println("password:" + loginUserDto.getPassword());
-        System.out.println("referer: " + referer);
-        return new ModelAndView("login");
-
-    } */
 }
