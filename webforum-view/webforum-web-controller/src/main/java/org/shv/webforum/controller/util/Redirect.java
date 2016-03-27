92c92406c99eed4c95c3a5fad33f21ff747b3cc5
Vladimir Sharapov <vladimirr.sharapov@gmail.com>
2016-03-27 19:46:37 +0300

    class to hold url redirect constansts used everywhere

---
+++ webforum-view/webforum-web-controller/src/main/java/org/shv/webforum/controller/util/Redirect.java
@@ -0,0 +1,23 @@
+/**
+ * This project is a simple web forum. I created it just to
+ * demonstrate my programming skills to potential employers.
+ *
+ * Here is short description: ( for more detailed description please reade README.md or
+ * go to https://github.com/VladimirSharapov/SpringWebForum )
+ *
+ * Front-end: jsp, bootstrap, jquery
+ * Back-end: Spring, Hibernate
+ * DB: MySQL and H2(for testing) were used while developing, but the project is database independent.
+ *     Though it must be a relational DB.
+ * Tools: git,maven,jenkins,nexus,liquibase.
+ *
+ * My LinkedIn profile: https://ru.linkedin.com/in/vladimir-sharapov-6075207
+ */
+package org.shv.webforum.controller.util;
+
+/**
+ * @author Vladimir Sharapov
+ */
+public class Redirect {
+    public static final String TOPIC_URL = "redirect:/topics/";
+}
