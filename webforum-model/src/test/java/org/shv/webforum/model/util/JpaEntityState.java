e418563e72dca1d359b158909d9e8872f6c5ed3c
Vladimir Sharapov <vladimirr.sharapov@gmail.com>
2016-04-01 02:04:53 +0300

    state of entity related to jpa lifecycle

---
+++ webforum-model/src/test/java/org/shv/webforum/model/util/JpaEntityState.java
@@ -0,0 +1,24 @@
+/**
+ * This project is a simple web forum. I created it just to
+ * demonstrate my programming skills to potential employers.
+ *
+ * Here is short description: ( for more detailed description please reade README.md or
+ * go to https://github.com/VladimirSharapov/SpringWebForum )
+ *
+ * Front-end: jsp, bootstrap, jquery
+ * Back-end: Spring, Hibernate
+ * DB: MySQL and H2(for testing) were used while developing, but the project is database independent.
+ *     Though it must be a relational DB.
+ * Tools: git,maven,jenkins,nexus,liquibase.
+ *
+ * My LinkedIn profile: https://ru.linkedin.com/in/vladimir-sharapov-6075207
+ */
+package org.shv.webforum.model.util;
+
+/**
+ * Enumeration describes entity states in jpa lifecycle. For different tests
+ * we need domain objects in different jpa states.
+ *
+ * @author Vladimir Sharapov
+ */
+public enum JpaEntityState {TRANSIENT, PERSISTENT, DETACHED }
