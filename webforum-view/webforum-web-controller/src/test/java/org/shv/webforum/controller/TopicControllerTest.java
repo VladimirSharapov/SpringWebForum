3c498595b3b0123b9db595be8e09d61d6c5577d1
Vladimir Sharapov <vladimirr.sharapov@gmail.com>
2016-04-22 17:13:24 +0300

    topic controller test

---
+++ webforum-view/webforum-web-controller/src/test/java/org/shv/webforum/controller/TopicControllerTest.java
@@ -0,0 +1,97 @@
+package org.shv.webforum.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import org.shv.webforum.model.entity.Post;
+import org.shv.webforum.model.entity.Topic;
+import org.shv.webforum.service.BranchService;
+import org.shv.webforum.service.PostService;
+import org.shv.webforum.service.TopicService;
+import org.shv.webforum.service.UserService;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+/**
+ * @author Vladimir Sharapov
+ */
+public class TopicControllerTest {
+
+    @Mock
+    private BranchService branchService;
+
+    @Mock
+    private TopicService topicService;
+
+    @Mock
+    private PostService postService;
+
+    @Mock
+    private UserService userService;
+
+    MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+        mockMvc = standaloneSetup(new TopicController(branchService,topicService,postService,userService)).build();
+    }
+
+
+    @Test
+    public void testShowPosts() throws Exception {
+        int pageNumber = 1;
+        long topicId = 1L;
+        long lastPostId = 1L;
+        Topic topic = createTopic();
+        Page<Post> expectedPage = createPage(1L);
+
+        when(topicService.get(topicId)).thenReturn(topic);
+        when(postService.getPostsInTopic(topic,pageNumber)).thenReturn(expectedPage);
+
+        mockMvc.perform(
+                get("/topics/{topicId}",topicId).param("page",Integer.toString(pageNumber)))
+                .andExpect(status().isOk())
+                .andExpect(view().name(TopicController.POST_LIST_VIEW))
+                .andExpect(model().attribute(TopicController.TOPIC_ATTR, topic))
+                .andExpect(model().attribute(TopicController.POST_PAGE_ATTR,expectedPage))
+                .andExpect(model().attribute(TopicController.LAST_POST_ID_ATTR, lastPostId));
+
+        verify(topicService,times(1)).get(topicId);
+        verify(postService,times(1)).getPostsInTopic(topic,pageNumber);
+    }
+
+    private Page<Post> createPage(long lastPostId) {
+        Post post = new Post();
+        post.setId(lastPostId);
+
+        List<Post> postList = new ArrayList<Post>();
+        postList.add(post);
+
+        return new PageImpl<Post>(postList);
+    }
+
+    private Topic createTopic() {
+        Topic topic = new Topic();
+        topic.setId(1L);
+        return topic;
+    }
+
+}
